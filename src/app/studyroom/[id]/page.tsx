2db4e95f9405fda8248a39f8dca22a66a8aa290a
Hitansh Doshi <hkd159@gmail.com>
2023-08-06 22:43:43 +0530

    added editor with default settings

--- src/app/studyroom/[id]/page.tsx
+++ src/app/studyroom/[id]/page.tsx
@@ -1,7 +1,10 @@
+import Editor from "@/components/Editor";
+
 export default function studyroom({ params }: { params: { id: string }}){
     return(
         <div>
             This is your studyroom id - {params.id}.
+            <Editor/>
         </div>
     )
 }
