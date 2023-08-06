ee2de87ae7c5b042d49d3a0b20f6355abd46739d
Harshit Daga <133854650+harshitdwebknot@users.noreply.github.com>
2023-08-07 01:26:00 +0530

    test commit

--- src/app/studyroom/[id]/page.tsx
+++ src/app/studyroom/[id]/page.tsx
@@ -1,9 +1,16 @@
 import Editor from "@/components/Editor";
 
 export default function studyroom({ params }: { params: { id: string }}){
+
+
+    const YoutubeVideoLink = ""
     return(
         <div>
+
             This is your studyroom id - {params.id}.
+            
+
+            
             <Editor/>
         </div>
     )
